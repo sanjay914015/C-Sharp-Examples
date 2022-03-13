1807f76bcffacf1ac403b4aa9c8e64f9af889e2e
sanjay914015 <sanjaychauhan914015@gmail.com>
2022-03-13 10:35:45 +0530

    Day 6 Tasks

---
+++ Day 6/Day 6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Day_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = @"C:\C Sharp\C-Sharp-Examples\Day 6\Day 6\test.txt";
+
+            System.IO.StreamReader reader;
+            reader = new System.IO.StreamReader(path);
+            string Data = reader.ReadToEnd();
+            reader.Close();
+
+            string[] values = Data.Split('*','>','~');
+            try
+            {
+                for(int i = 0; i < values.Length; i++)
+                {
+                    Console.WriteLine(values[i]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("No More Data Available !!!");
+            }
+        }
+    }
+}
---
+++ Today Project/Today Project/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Today_Project
+{
+    internal class Program
+    {
+        static int tableWidth = 73;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Admin \nUser ");
+            string answer = Console.ReadLine();
+            ArrayList userList = new ArrayList();
+            double MobileNo = 0;
+
+            switch (answer)
+            {
+                case "User":
+                    while (true)
+                    {
+                        Console.WriteLine("SignIN\nLogIN\nForgotPassword\nExit");
+                        string input = Console.ReadLine();
+                        if (input == "SignIN")
+                        {
+                            Console.WriteLine("Enter Name :");
+                            string Name = Console.ReadLine();
+                            try
+                            {
+                                Console.WriteLine("Enter Mobile NO :");
+                                MobileNo = Convert.ToDouble(Console.ReadLine());
+                                userList.Add(MobileNo);
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Please Enter valid Mobile Number");
+                                continue;
+                            }
+                            Console.WriteLine("Enter Secret Question and Answer:");
+                            string question = Console.ReadLine();
+                            string secretanswer = Console.ReadLine();
+                            string password = Name + "@123";
+                            userList.Add(password);
+                            userList.Add(question);
+                            userList.Add(secretanswer);
+                            Console.WriteLine("Your Username is :" + MobileNo + " Password :" + password);
+                            continue;
+
+                        }
+                        else if (input == "LogIN")
+                        {
+                            Console.WriteLine("Enter UserName :");
+                            double enterusername = Convert.ToDouble(Console.ReadLine());
+                            Console.WriteLine("Enter Password :");
+                            string enterpassword = Console.ReadLine();
+
+                            if (enterusername == Convert.ToDouble(userList[0]) && enterpassword == Convert.ToString(userList[1]))
+                            {
+                                Console.WriteLine("Enter New password");
+                                string password = Console.ReadLine();
+
+                                userList[1] = password;
+                                Console.WriteLine("UserName : " + userList[0] + " Password : " + userList[1]);
+                            }
+                            continue;
+                        }
+                        else if (input == "ForgotPassword")
+                        {
+                            Console.WriteLine("Enter Username");
+                            double username = Convert.ToDouble(Console.ReadLine());
+                            if (username == Convert.ToDouble(userList[0]))
+                            {
+                                Console.WriteLine("Answer the secret Question :");
+                                Console.WriteLine(userList[2]);
+                                string Questionanswer = Console.ReadLine();
+
+                                if (Questionanswer == Convert.ToString(userList[3]))
+                                {
+                                    Console.WriteLine("Enter New Password");
+                                    string password = Console.ReadLine();
+                                    userList[1] = password;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("User Doesn't Exists !!");
+                                }
+                                Console.WriteLine("UserName : " + userList[0] + " Password : " + userList[1]);
+                            }
+                        }
+                        else if (input == "Exit")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+
+                    }
+                    break;
+                case "Admin":
+                    while (true)
+                    {
+                        Console.WriteLine("LogIN\nForgotPassword\nExit");
+                        string input = Console.ReadLine();
+                        if (input == "LogIN")
+                        {
+                            Console.WriteLine("Enter UserName :");
+                            string enterusername = Console.ReadLine();
+                            Console.WriteLine("Enter Password :");
+                            string enterpassword = Console.ReadLine();
+
+                            if (enterusername == "Admin" && enterpassword == "Admin")
+                            {
+                                Console.WriteLine("UserName : " + enterusername + " Password : " + enterpassword);
+                                while (true)
+                                {
+                                    Console.WriteLine("1. User Approval 2. Exit");
+                                    int asw = Convert.ToInt32(Console.ReadLine());
+                                    if (asw == 1)
+                                    {
+                                        Console.WriteLine("List OF User");
+                                        Console.Clear();
+                                        PrintLine();
+                                        PrintRow("ID", "From Port", "To Port", "Request");
+                                        PrintLine();
+                                        PrintRow("", "", "", "");
+                                        PrintRow("", "", "", "");
+                                        PrintLine();
+                                        Console.ReadLine();
+
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Please Enter Valid Username and Password ::");
+                                continue;
+                            }
+
+
+                        }
+                        else if (input == "ForgotPassword")
+                        {
+
+                        }
+                        else if (input == "Exit")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    break;
+                default:
+                    {
+                        Console.WriteLine("Invalid Input ::");
+                        break;
+                    }
+            }
+        }
+        static void PrintLine()
+        {
+            Console.WriteLine(new string('-', tableWidth));
+        }
+
+        static void PrintRow(params string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (string column in columns)
+            {
+                row += AlignCentre(column, width) + "|";
+            }
+
+            Console.WriteLine(row);
+        }
+
+        static string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+    }
+}
